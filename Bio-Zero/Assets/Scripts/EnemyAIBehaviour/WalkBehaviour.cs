da73668d77fd28c83c489b61ef26711ea768136d
Francesco Congiu <f.congiu38@studenti.unica.it>
2023-03-14 22:45:31 +0100

    Fixed AI Enemy Movement with Alert, Follow and Attack Mode using Unity Animator

--- Bio-Zero/Assets/Scripts/EnemyAIBehaviour/WalkBehaviour.cs
+++ Bio-Zero/Assets/Scripts/EnemyAIBehaviour/WalkBehaviour.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class WalkBehaviour : StateMachineBehaviour
 {
-    private float timer;
-    
+
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timer = 0;
+ 
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timer += Time.deltaTime;
-
-        if (timer > 10)
-        {
-            animator.SetBool("isWalking", false);
-        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
--- Bio-Zero/Assets/Scripts/EnemyMovement.cs
+++ Bio-Zero/Assets/Scripts/EnemyMovement.cs
@@ -1,17 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private NavMeshAgent enemy;
+    public Transform[] pathEnemy; //Transform path for Enemy
+    private int pathEnemyIndex;
+    private int index;
+    private Vector3 actualCpTarget;
 
-    private Animator enemyState;
+    private NavMeshAgent enemy; //Enemy object
+    private Animator enemyState; //States Animation Enemy
 
     private float distanceEnemyPlayer;
-    
-    private float rangeAlert = 15.0f;
-    private float rangeAttack = 03.0f;
+    private bool flag;
 
+    private float rangeAlert = 10.0f;
+    private float rangeAttack = 01.0f;
+    
+    private float waitTime;
+    private float initWaitTime = 1f;
+    
     public GameObject playerTarget;
     // Start is called before the first frame update
     void Start()
@@ -20,21 +30,55 @@ public class EnemyMovement : MonoBehaviour
         playerTarget = GameObject.FindWithTag("Player");
         enemyState = GetComponent<Animator>();
 
+        waitTime = initWaitTime;
+        pathEnemyIndex = Random.Range(0, pathEnemy.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
         ResetAnimatorState(enemyState);
-        enemy.isStopped = false;
-        
+        float distance = Vector3.Distance(playerTarget.transform.position, transform.position);
+
+        if (distance > rangeAlert)
+        {
+            flag = false;
+            idleStateMode();
+        }
+        else
+        {
+            flag = true;
+            followPlayer();
+        }
+    }
+
+    private void idleStateMode()
+    {
+        enemy.SetDestination(pathEnemy[pathEnemyIndex].position);
+
+        if (Vector3.Distance(transform.position, pathEnemy[pathEnemyIndex].position) < 3.0f)
+        {
+            if (waitTime <= 0)
+            {
+                enemyState.SetBool("isWalking", true);
+                pathEnemyIndex = Random.Range(0, pathEnemy.Length);
+                waitTime = initWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void followPlayer()
+    {
         //Set distance offset between enemy ai and player
-        distanceEnemyPlayer = Vector3.Distance(enemy.transform.position, playerTarget.transform.position);
+        distanceEnemyPlayer = Vector3.Distance(transform.position, playerTarget.transform.position);
         
         if (distanceEnemyPlayer < rangeAttack)
         { //Distance between Enemy and Player is lower than 1
             enemyState.SetBool("isAttack", true);
-            enemy.SetDestination(playerTarget.transform.position);
         }
         
         else if (distanceEnemyPlayer < rangeAlert)
@@ -42,19 +86,12 @@ public class EnemyMovement : MonoBehaviour
             enemyState.SetBool("isAlert", true);
             enemy.SetDestination(playerTarget.transform.position);
         }
-
-        else
-        { //Distance between Enemy and Player is lower than 1
-            enemyState.SetBool("isWalking", true);
-            enemy.isStopped = false;
-        }
-        
     }
 
     public void ResetAnimatorState(Animator animator)
     {
-        animator.SetBool("isIdle", false);
         animator.SetBool("isAlert", false);
         animator.SetBool("isAttack", false);
     }
+
 }
