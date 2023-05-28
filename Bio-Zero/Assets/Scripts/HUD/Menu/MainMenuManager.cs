4b564b4fa55a339a9aad557af6032aee1f20ef6b
Francesco Congiu <f.congiu38@studenti.unica.it>
2023-05-28 10:22:22 +0200

    I've fixed and added some features be like:
    - HUD and Logic Menu for choose Level from Start;

--- Bio-Zero/Assets/Scripts/HUD/Menu/MainMenuManager.cs
+++ Bio-Zero/Assets/Scripts/HUD/Menu/MainMenuManager.cs
@@ -1,6 +1,8 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
+using Toggle = UnityEngine.UI.Toggle;
 
 namespace HUD.Menu
 {
@@ -13,19 +15,27 @@ namespace HUD.Menu
         public GameObject loadGameMenu;
         public GameObject optionsMenu;
         public GameObject infoMenu;
+        public GameObject chooseLevelMenu;
 
         [Header("Button Actions")] 
         public GameObject buttonStartGame;
         public TextMeshProUGUI insertNickname;
         public SceneLoader sceneM;
-    
-        public GameObject buttonLoadGame;
+
+        [Header("Toggle Level")] 
+        private int levelChoose;
+        public Toggle isActiveFirst;
+        public Toggle isActiveSecond;
+        public Toggle isActiveThird;
+        public TextMeshProUGUI insertNicknameLevelChoose;
         
         public void Start()
         {
+            levelChoose = 1;
             Time.timeScale = 1f;
             newGameMenu.SetActive(false);
             loadGameMenu.SetActive(false);
+            chooseLevelMenu.SetActive(false);
             optionsMenu.SetActive(false);
             infoMenu.SetActive(false);
         }
@@ -39,6 +49,12 @@ namespace HUD.Menu
         {
             DataManager.DataManager.instance.LoadGame(insertNickname.text);
             sceneM.ChangeScene(1);
+        }
+
+        public void StartGameFromLevelChoose()
+        {
+            DataManager.DataManager.instance.LoadGame(insertNicknameLevelChoose.text);
+            sceneM.ChangeScene(levelChoose);
         }
 
         public void LoadOldGame(TextMeshProUGUI nicknameTextField)
@@ -56,16 +72,37 @@ namespace HUD.Menu
 
         public void LoadGame()
         {
+            newGameMenu.SetActive(false);
             loadGameMenu.SetActive(true);
+            chooseLevelMenu.SetActive(false);
+            optionsMenu.SetActive(false);
+            infoMenu.SetActive(false);
+        }
+
+        public void ChooseLevel()
+        {
+            newGameMenu.SetActive(false);
+            loadGameMenu.SetActive(false);
+            chooseLevelMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+            infoMenu.SetActive(false);
         }
 
         public void Options()
         {
+            newGameMenu.SetActive(false);
+            loadGameMenu.SetActive(false);
+            chooseLevelMenu.SetActive(false);
             optionsMenu.SetActive(true);
+            infoMenu.SetActive(false);
         }
 
         public void Info()
         {
+            newGameMenu.SetActive(false);
+            loadGameMenu.SetActive(false);
+            chooseLevelMenu.SetActive(false);
+            optionsMenu.SetActive(false);
             infoMenu.SetActive(true);
         }
 
@@ -74,9 +111,25 @@ namespace HUD.Menu
             newGameMenu.SetActive(false);
             //Add feature for reset input type
             insertNickname.text = "";
+            chooseLevelMenu.SetActive(false);
             loadGameMenu.SetActive(false);
             optionsMenu.SetActive(false);
             infoMenu.SetActive(false);
+        }
+        
+        public void firstLevelRadioMethod()
+        {
+            levelChoose = 1;
+        }
+    
+        public void secondLevelRadioMethod()
+        {
+            levelChoose = 2;
+        }
+    
+        public void thirdLevelRadioMethod()
+        {
+            levelChoose = 3;
         }
     }
 }
